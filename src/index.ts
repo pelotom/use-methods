c7fc5b6912de1ae179a2dde13f90b037eea7b852
Tom Crockett <pelotom@gmail.com>
2019-03-26 17:47:21 -0700

    Improve type inference and fix unsoundness where methods could return any type

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,48 @@
+import { Draft } from 'immer';
 import { useCallback, useMemo } from 'react';
 import { useImmerReducer } from 'use-immer';
-import { Draft } from 'immer';
-
-export type Methods<
-  S extends object = any,
-  R extends Record<string, (...args: any[]) => S | void> = any
-> = (s: S) => R;
-
-export type StateFor<M extends Methods> = M extends Methods<infer S> ? S : never;
-
-export type ActionUnionFor<M extends Methods> = M extends Methods<any, infer R>
-  ? { [T in keyof R]: { type: T; payload: Parameters<R[T]> } }[keyof R]
-  : never;
-
-export type CallbacksFor<M extends Methods> = {
-  [T in ActionUnionFor<M>['type']]: (
-    ...payload: ActionByType<ActionUnionFor<M>, T>['payload']
-  ) => void
-};
-
-export type ActionByType<A, K> = A extends { type: infer K2 } ? (K extends K2 ? A : never) : never;
 
 export type StateAndCallbacksFor<M extends Methods> = StateFor<M> & CallbacksFor<M>;
 
-export default function useStateMethods<M extends Methods>(
-  initialState: StateFor<M>,
-  methods: M,
-): StateAndCallbacksFor<M> {
+export type StateFor<M extends Methods> = M extends Methods<infer S, any> ? S : never;
+
+export type CallbacksFor<M extends Methods> = M extends Methods<any, infer R>
+  ? {
+      [T in ActionUnion<R>['type']]: (
+        ...payload: ActionByType<ActionUnion<R>, T>['payload']
+      ) => void
+    }
+  : never;
+
+export type Methods<S extends object = any, R extends MethodRecordBase<S> = any> = (state: S) => R;
+
+export type MethodRecordBase<S extends object = any> = Record<string, (...args: any[]) => S | void>;
+
+export type ActionUnion<R extends MethodRecordBase> = {
+  [T in keyof R]: { type: T; payload: Parameters<R[T]> }
+}[keyof R];
+
+export type ActionByType<A, T> = A extends { type: infer T2 } ? (T extends T2 ? A : never) : never;
+
+export default function useStateMethods<S extends object, R extends MethodRecordBase<S>>(
+  initialState: S,
+  methods: Methods<S, R>,
+): StateAndCallbacksFor<typeof methods> {
   const reducer = useCallback(
-    (state: Draft<StateFor<M>>, action: ActionUnionFor<M>) =>
-      methods(state)[action.type](...action.payload),
+    (state: Draft<S>, action: ActionUnion<R>) =>
+      methods(state as S)[action.type](...action.payload),
     [methods],
   );
   const [state, dispatch] = useImmerReducer(reducer, initialState);
-  const actionTypes: ActionUnionFor<M>['type'][] = Object.keys(methods(initialState));
+  const actionTypes: ActionUnion<R>['type'][] = Object.keys(methods(initialState));
   const callbacks = useMemo(
     () =>
       actionTypes.reduce(
         (accum, type) => {
-          accum[type] = (...payload) => dispatch({ type, payload } as ActionUnionFor<M>);
+          accum[type] = (...payload) => dispatch({ type, payload } as ActionUnion<R>);
           return accum;
         },
-        {} as CallbacksFor<M>,
+        {} as CallbacksFor<typeof methods>,
       ),
     actionTypes,
   );
